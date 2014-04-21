96f299df37aaab11b9077e59ec9d597203ed9d2e
alji2106 <alix.jimenez@live.com>
2014-04-20 22:54:57 -0600

    Finish sign up and Sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to FakeBook!"
-      redirect_to user_path(:id => @user.id)
+      redirect_to @user
     else
       render 'new'
     end
