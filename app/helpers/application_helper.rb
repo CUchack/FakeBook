b67cf138d2388a8866a146abe598fab880918db2
CUchack <Christopher.Hack@colorado.edu>
2014-04-27 18:43:55 -0600

    Integrating old views into new working framework

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "FakeBook"
+    base_title = "Welcome to FakeBook"
     if page_title.empty?
       base_title
     else
