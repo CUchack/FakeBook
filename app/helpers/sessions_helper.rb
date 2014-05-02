77aeb8c3e79ef50c9136a03c25bcbb6fb78afb1e
CUchack <Christopher.Hack@colorado.edu>
2014-05-01 20:22:34 -0600

    looks ok for now

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -45,4 +45,17 @@ module SessionsHelper
   def store_location
     session[:return_to] = request.url if request.get?
   end
+  
+  def my_feed(user)
+    @self = user[:id]
+    @friendslist = [@self,2,3,5,1,4,6] #change this to user.friends when friends are implemented
+    @postlist = []
+    for friend in @friendslist do
+      @post = [Micropost.find_by_user_id(friend)]
+      @postlist.concat(@post)
+    end
+    @postlist.sort{ |x,y| y.created_at <=> x.created_at}
+    @postlist[0..10]
+  end
+
 end
