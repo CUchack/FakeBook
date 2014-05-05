58fbd2f760eaaf02ad4f23c0a698310fdd0b63d7
LJFrog <lauren.jensen@colorado.edu>
2014-05-05 00:27:28 -0600

    committing changes to pull

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140504205419) do
+ActiveRecord::Schema.define(version: 20140504051534) do
 
   create_table "friendships", force: true do |t|
     t.integer "friendable_id"
@@ -25,13 +25,6 @@ ActiveRecord::Schema.define(version: 20140504205419) do
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "relationships", force: true do |t|
-    t.integer  "follower_id"
-    t.integer  "followed_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
