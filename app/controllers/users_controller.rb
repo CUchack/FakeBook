94d2887d934695bbf63e3a2f79f7170232b8b901
CUchack <Christopher.Hack@colorado.edu>
2014-05-05 00:13:01 -0600

    fixed profile edit page so that you don't need to enter a password every time you make a profile edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,6 +50,7 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+
   end
   
 #  def clearwall
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ActiveRecord::Base
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
     has_secure_password
-    validates :password, length: { minimum: 6 }
+    validates :password, length: { minimum: 6 }, :unless => Proc.new {|u| u.password.blank?}
     def User.new_remember_token
       SecureRandom.urlsafe_base64
     end
